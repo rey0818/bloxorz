ad7ad5ad81e73ebda9d1cc5976d7104e4e344f58
rey <114591586+rey0818@users.noreply.github.com>
2023-12-12 18:58:18 +0800

    change1
    few changes

--- script.js
+++ script.js
@@ -1,54 +1,60 @@
+var  playerX = 0 ;
+var playerY = 0 ;
+var canvas = document.getElementById('gameCanvas');
+var ctx = canvas.getContext('2d');
 function createGrid() {
     var grid = [];
-    for (var i = 0; i < 8; i++) {
+    for (var i = 0; i < 10; i++) {
        grid[i] = [];
-       for (var j = 0; j < 8; j++) {
-         grid[i][j] = 1;
+       for (var j = 0; j < 10; j++) {
+         if(i&1) grid[i][j] = 1;
        }
     }
     return grid;
    }
+   function drawGrid(grid) {
+      for (var i = 0; i < 10; i++) {
+         for (var j = 0; j < 10; j++) {
+           if(grid[i][j]==2) ctx.fillStyle = 'Red' ;
+           else if (grid[i][j] == 1) {
+             ctx.fillStyle = 'black';
+           } else {
+             ctx.fillStyle = 'white';
+           }
+           ctx.fillRect(i * 80, j * 40, 80, 40);
+         }
+      }
+     }
 // Function to handle block movement
-function moveBlock(grid, x, y, direction) {
-    // Your logic for block movement goes here
-   }
    document.addEventListener('keydown', function(event) {
-    if (event.key == 37) { // Move left
+    if (event.keyCode == 37) { // Move left
        moveBlock(grid, playerX, playerY, [-1, 0]);
-    } else if (event.key == 38) { // Move up
+       console.log("hi");
+    } else if (event.keyCode == 38) { // Move up
        moveBlock(grid, playerX, playerY, [0, -1]);
-    } else if (event.key == 39) { // Move right
+    } else if (event.keyCode == 39) { // Move right
        moveBlock(grid, playerX, playerY, [1, 0]);
-    } else if (event.key == 40) { // Move down
+    } else if (event.keyCode == 40) { // Move down
        moveBlock(grid, playerX, playerY, [0, 1]);
     }
+    drawGrid(grid);
    });
    canvas.addEventListener('click', function(event) {
     // Your logic for handling mouse clicks goes here
    });
-   function drawGrid(grid) {
-    for (var i = 0; i < 8; i++) {
-       for (var j = 0; j < 8; j++) {
-         if (grid[i][j] == 1) {
-           ctx.fillStyle = 'black';
-         } else {
-           ctx.fillStyle = 'white';
-         }
-         ctx.fillRect(j * 100, i * 100, 100, 100);
-       }
-    }
-   }
    function moveBlock(grid, x, y, direction) {
     var newX = x + direction[0];
     var newY = y + direction[1];
    
-    if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
-       grid[newY][newX] = grid[y][x];
+    if (newX >= 0 && newX < 10  && newY >= 0 && newY < 10 ) {
        grid[y][x] = 0;
+       grid[newY][newX] = 2 ;
     }
+    playerX = newX ;
+    playerY = newY ;
    }
    function switchBlock(grid, x, y) {
     grid[y][x] = grid[y][x] == 0 ? 1 : 0;
    }
-createGrid() ;
+var grid = createGrid() ;
 drawGrid(grid);
